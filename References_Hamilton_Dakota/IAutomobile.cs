49f734164125b60090c283d23ad9b2326807d3ac
DakotaHamilton <113059394+DakotaHamilton@users.noreply.github.com>
2023-02-06 15:12:14 -0600

    Add project files.

---
+++ References_Hamilton_Dakota/IAutomobile.cs
@@ -0,0 +1,22 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace References_Hamilton_Dakota
+{
+    /// <summary>
+    /// IAutomobile Interface
+    /// </summary>
+    interface IAutomobile
+    {
+
+        public double Speed { get; }
+
+        public int Wheels { get; }
+
+        public string LicensePlate { get; }
+
+        public void Stringify();
+
+    }
+}
---
+++ References_Hamilton_Dakota/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace References_Hamilton_Dakota
+{
+    class Program
+    {
+        /// <summary>
+        /// Prints Information from references
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Sedan myFirstSedan = new Sedan(0);
+            IAutomobile myAutomobile = myFirstSedan;
+
+            Sedan myOtherSedan = new Sedan(0);
+
+            myFirstSedan.IncreaseSpeed();
+            Console.WriteLine(myFirstSedan.Speed);
+            Console.WriteLine(myAutomobile.Speed);
+            Console.WriteLine(myFirstSedan.Equals(myAutomobile));
+
+            myOtherSedan.IncreaseSpeed();
+            Console.WriteLine(myFirstSedan.Speed);
+            Console.WriteLine(myOtherSedan.Speed);
+            Console.WriteLine(myFirstSedan.Equals(myOtherSedan));
+
+            Truck myTruck = new Truck(50, 500, "MyTrUck");
+
+            // Calls stringify method and describes automobiles.
+            myFirstSedan.Stringify();
+            myAutomobile.Stringify();
+            myOtherSedan.Stringify();
+            myTruck.Stringify();
+        }
+    }
+}
